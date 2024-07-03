ad953ec87f84efcd4240af1f8ca4298a64f1a85b
Gopal Mittal <gopal.mittal0@gmail.com>
2024-07-03 16:31:40 +0530

    codegen file

--- backend_amp/src/graphql/mutations.js
+++ backend_amp/src/graphql/mutations.js
@@ -46,3 +46,51 @@ export const deleteTodo = /* GraphQL */ `
     }
   }
 `;
+export const createUserNotification = /* GraphQL */ `
+  mutation CreateUserNotification(
+    $input: CreateUserNotificationInput!
+    $condition: ModelUserNotificationConditionInput
+  ) {
+    createUserNotification(input: $input, condition: $condition) {
+      id
+      name
+      description
+      notificationType
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateUserNotification = /* GraphQL */ `
+  mutation UpdateUserNotification(
+    $input: UpdateUserNotificationInput!
+    $condition: ModelUserNotificationConditionInput
+  ) {
+    updateUserNotification(input: $input, condition: $condition) {
+      id
+      name
+      description
+      notificationType
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteUserNotification = /* GraphQL */ `
+  mutation DeleteUserNotification(
+    $input: DeleteUserNotificationInput!
+    $condition: ModelUserNotificationConditionInput
+  ) {
+    deleteUserNotification(input: $input, condition: $condition) {
+      id
+      name
+      description
+      notificationType
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
