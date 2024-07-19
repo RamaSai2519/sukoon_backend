76def8732cbe9aa2c1220b73d12abbdab8826541
Gopal Mittal <gopal.mittal0@gmail.com>
2024-07-19 15:11:48 +0530

    added scheduled call handler

--- backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
+++ backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
@@ -1,12 +1,32 @@
-
+import requests
+import json
 from models.iprocessor import IProcessor
 
 class CallJobHandler(IProcessor):
-    def __init__(self):
-        pass
+    def __init__(self, job):
+        self.request_meta = json.loads(job.get("requestMeta")) or {}
+
+    def invoke_call_backend_api(self):
+        url = "https://prod-backend.sukoonunlimited.com/api/call/backend-make-call"
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        }
+
+        payload = json.dumps({
+            "userId": self.request_meta.get("userId"),
+            "expertId": self.request_meta.get("expertId"),
+        })
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text)
+
+        return response.text
 
 
     def process_scheduled_job(self):
+        response = self.invoke_call_backend_api()
 
-        return None
+        return response
 
