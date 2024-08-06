9df2b96736557ee9968c63efd18c6f4564505fac
Gopal Mittal <gopal.mittal0@gmail.com>
2024-08-06 13:42:54 +0530

    updating status

--- backend_amp/amplify/backend/function/scheduler/src/index.py
+++ backend_amp/amplify/backend/function/scheduler/src/index.py
@@ -4,52 +4,53 @@ from queries.scheduled_job import get_pending_scheduled_jobs, mark_my_job_as_pic
 
 
 def construct_response(statusCode, body):
-   response = {
-       "statusCode": statusCode,
-       "headers": {
-           "Content-Type": "application/json",
-           "Access-Control-Allow-Origin": "*",
-           "Access-Control-Allow-Headers": "*",
-           "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-       },
-       "body": json.dumps(body),
-   }
-   return response
+    response = {
+        "statusCode": statusCode,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps(body),
+    }
+    return response
 
 def handler(event, context):
-   try:
-       print(f"Event: {event}")
-       print(f"Context: {context}")
+    try:
+        print(f"Event: {event}")
+        print(f"Context: {context}")
 
-       time = event["time"]
-       status = "PENDING"
-       next_token = None
-       first_time = True
+        time = event["time"]
+        status = "PENDING"
+        next_token = None
+        first_time = True
 
-       while next_token or first_time:
-           data = get_pending_scheduled_jobs(
+        while next_token or first_time:
+            data = get_pending_scheduled_jobs(
                status=status, time=time, next_token=next_token
-           )["scheduledJobsByStatusAndTime"]
+            )["scheduledJobsByStatusAndTime"]
+           
+            jobs = data["items"]
+            print(jobs)
+            for job in jobs:
+                try:
+                    if job.get("scheduledJobType"):
+                        controller.process(job)
+                    else:
+                        print(f"no job type for given job {job}")
+                except Exception as error:
+                    print(
+                        f"error in executing processor for job {job} with error {error}"
+                    )
+                if job.get("id"):
+                    mark_my_job_as_picked(job.get("id", None))
+            first_time = False
+            next_token = data["nextToken"]
 
-           jobs = data["items"]
-           for job in jobs:
-               try:
-                   if job.get("scheduledJobType"):
-                       controller.process(job)
-                   else:
-                       print(f"no job type for given job {job}")
-               except Exception as error:
-                   print(
-                       f"error in executing processor for job {job} with error {error}"
-                   )
-               if job.get("id"):
-                   mark_my_job_as_picked(job.get("id", None))
-           first_time = False
-           next_token = data["nextToken"]
-
-   except Exception as error:
-       print(
-           f"error in getting scheduled job for time {time} and status {status} error {error}"
-       )
-       return construct_response(statusCode=400, body={})
-   return construct_response(statusCode=200, body={})
+    except Exception as error:
+        print(
+            f"error in getting scheduled job for time {time} and status {status} error {error}"
+        )
+        return construct_response(statusCode=400, body={})
+    return construct_response(statusCode=200, body={})
--- backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
+++ backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
@@ -1,10 +1,12 @@
 import requests
 import json
 from models.iprocessor import IProcessor
+from queries.scheduled_job import update_scheduled_job_status
 
 class CallJobHandler(IProcessor):
     def __init__(self, job):
         self.request_meta = json.loads(job.get("requestMeta")) or {}
+        self.job_id = job.get("id", "")
 
     def invoke_call_backend_api(self):
         url = "https://prod-backend.sukoonunlimited.com/api/call/backend-make-call"
@@ -23,11 +25,18 @@ class CallJobHandler(IProcessor):
         response = requests.request("POST", url, headers=headers, data=payload)
         print(response.text)
 
-        return response.text
-
+        return response
 
     def process_scheduled_job(self):
         response = self.invoke_call_backend_api()
+        json_response = response.json()
+        success_status = json_response.get("success")
+        
+        if not success_status:
+            status = ((json_response.get("error", {}) or {}).get("message", {}) or {}).get("message", "")
+        else:
+            status = "Call Placed Successfully"
+        update_scheduled_job_status(self.job_id, status)
 
         return response
 
