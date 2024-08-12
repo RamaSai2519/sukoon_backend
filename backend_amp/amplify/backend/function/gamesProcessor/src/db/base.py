427b8530688654ee890887264bde45969cca93a0
Gopal Mittal <gopal.mittal0@gmail.com>
2024-08-12 17:36:01 +0530

    fixes

--- backend_amp/amplify/backend/function/gamesProcessor/src/db/base.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/db/base.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
+import certifi
 from configs import CONFIG as Config
+
 
 class Database:
     _instance = None
@@ -7,6 +9,6 @@ class Database:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
-            cls._instance.client = MongoClient(Config.DB_CONFIG.get("connection_url"))
+            cls._instance.client = MongoClient(Config.DB_CONFIG.get("connection_url"),  tlsCAFile=certifi.where())
         return cls._instance
 
