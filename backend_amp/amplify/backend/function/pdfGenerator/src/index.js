c691b982d3c477c378fd614ca60528d588a77e01
Mayank Anil Dwivedi <mayankanildwivedi@gmail.com>
2024-08-17 01:26:04 +0530

    Added PDF genration

--- backend_amp/amplify/backend/function/pdfGenerator/src/index.js
+++ backend_amp/amplify/backend/function/pdfGenerator/src/index.js
@@ -1,3 +1,4 @@
+const { generatePDF, uploadPDFToS3 } = require('./generatePdf');
 
 
 /**
@@ -5,6 +6,14 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    const pdfBuffer = await generatePDF(htmlContent);
+
+        // Define S3 parameters
+        const bucketName = 'sukoon-media';
+        const key = 'invoicePdf/output.pdf';
+
+        // Upload the PDF to S3
+        await uploadPDFToS3(pdfBuffer, bucketName, key);
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
