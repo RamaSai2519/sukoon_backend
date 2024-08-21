e64d722d402f14541778810c98230f5ee870f97c
Rama Sathya Sai <rchandik@gitam.in>
2024-08-21 21:46:00 +0530

    chore: Refactor imports in controller.py
    Refactor the imports in `controller.py` to improve code organization and readability.

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
@@ -1,12 +1,12 @@
-from services.src.game_config import GameConfigService
-from services.src.game import QuizGameService, CardGameService, ScoreUpdaterService, CalculateWinnerService,  GameHistoryService, BytePlusTokenService
-from services.src.coupon_reward import CouponRewardService
-from services.src.user_referral import UserReferralService
-from services.src.authenticate import SendOTPService,  ValidateOTPService
 from services.src.shorts import FetchShortsService
-from services.src.whatsapp import WhatsappMessageService, WhatsappWebhookService, WhatsappWebhookEvent
+from services.src.game_config import GameConfigService
+from services.src.cashfree import CashfreeWebhookService
+from services.src.user_referral import UserReferralService
+from services.src.coupon_reward import CouponRewardService
+from services.src.payment import CreatePaymentOrderService
+from services.src.events import CreateEventsService, UpdateEventService
+from services.src.authenticate import SendOTPService, ValidateOTPService
 from services.src.push_notification import PushNotificationService, FCMTokenService
 from services.src.scheduled_jobs import CreateScheduledJobsService, UpdateScheduledJobsService
-from services.src.events import CreateEventsService, UpdateEventService
-from services.src.payment import CreatePaymentOrderService
-from services.src.cashfree import CashfreeWebhookService
+from services.src.whatsapp import WhatsappMessageService, WhatsappWebhookService, WhatsappWebhookEvent
+from services.src.game import QuizGameService, CardGameService, ScoreUpdaterService, CalculateWinnerService, GameHistoryService, BytePlusTokenService
