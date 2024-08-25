0d5b758767a8fd82102736e12896fbdd00cdcf27
Rama Sathya Sai <rchandik@gitam.in>
2024-08-25 14:31:03 +0530

    refactor: Update routes in index.py

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/src/user.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/src/user.py
@@ -1,12 +1,7 @@
 import json
 import dataclasses
-from flask_restful import Resource
 from flask import request
-from models.interfaces import UpsertRegisteredUserInput
-from models.upsert_registered_user.main import UpsertRegisteredUser
-
-from models.interfaces import CreateNonRegisteredUserInput
-from models.create_non_registered_user.main import CreateNonRegisteredUser
+from flask_restful import Resource
 
 from models.interfaces import GetUserInput
 from models.get_user.main import GetUser
@@ -18,23 +13,15 @@ class UserService(Resource):
     
     def post(self) -> Output:
         input = json.loads(request.get_data())
-        input = UpsertRegisteredUserInput(**input)
-        output = UpsertRegisteredUser(input).process()
-        output = dataclasses.asdict(output)
-
-        return output
-    
-    def post(self) -> Output:
-        input = json.loads(request.get_data())
-        input = CreateNonRegisteredUserInput(**input)
-        output = CreateNonRegisteredUser(input).process()
-        output = dataclasses.asdict(output)
-
-        return output
-    
-    def post(self) -> Output:
-        input = json.loads(request.get_data())
         input = GetUserInput(**input)
+        output = GetUser(input).process()
+        output = dataclasses.asdict(output)
+
+        return output
+
+    def get(self) -> Output:
+        input_params = request.args
+        input = GetUserInput(**input_params)
         output = GetUser(input).process()
         output = dataclasses.asdict(output)
 
