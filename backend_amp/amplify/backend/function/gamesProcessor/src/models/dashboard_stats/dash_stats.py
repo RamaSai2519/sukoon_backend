23fd5e054d99975bac4726d802bbf6e366422ad0
Rama Sathya Sai <rchandik@gitam.in>
2024-08-29 18:39:25 +0530

    refactor: Improve handling of previous user data in compute.py

---
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/dashboard_stats/dash_stats.py
@@ -0,0 +1,107 @@
+from typing import Tuple
+from models.common import Common
+from helpers.experts import ExpertsHelper
+from db.calls import get_calls_collection
+from models.constants import successful_calls_query
+
+
+class DashboardStats:
+    def __init__(self, today_query: dict) -> None:
+        self.common = Common()
+        self.today_query = today_query
+        self.experts_helper = ExpertsHelper()
+        self.calls_collection = get_calls_collection()
+        self.total_successful_calls, self.total_duration = self._total_successful_calls_and_duration_()
+
+    def _total_successful_calls_and_duration_(self) -> Tuple[int, int]:
+        query = {**successful_calls_query, "duration": {"$exists": True}}
+        projection = {"duration": 1, "_id": 0}
+        successful_calls_data = self.common.get_calls(
+            query, projection, False, False)
+
+        total_seconds = [
+            Common.duration_str_to_seconds(call["duration"])
+            for call in successful_calls_data
+            if Common.duration_str_to_seconds(call["duration"]) > 60
+        ]
+
+        return len(total_seconds), sum(total_seconds)
+
+    def _get_successful_scheduled_calls_(self) -> list:
+        query = {**successful_calls_query,
+                 "$or": [{"type": "scheduled"}, {"scheduledId": {"$exists": True}}]}
+        projection = {"duration": 1, "scheduledId": 1, "type": 1, "_id": 0}
+        calls = self.common.get_calls(query, projection, False)
+
+        for call in calls:
+            seconds = Common.duration_str_to_seconds(
+                call["duration"]) if "duration" in call else 0
+            if seconds < 60:
+                calls.remove(call)
+        return calls
+
+    def _get_avg_conversation_score_(self) -> float:
+        query = {"Conversation Score": {
+            "$exists": True, "$gt": 0, "$ne": None}}
+        projection = {"Conversation Score": 1, "_id": 0}
+        calls = self.common.get_calls(query, projection, False, False)
+        scores = [call["Conversation Score"] for call in calls]
+        return round(sum(scores) / len(scores), 2) if scores else 0
+
+    def total_calls(self) -> int:
+        return self.calls_collection.count_documents({})
+
+    def today_calls(self) -> int:
+        return self.calls_collection.count_documents(self.today_query)
+
+    def successful_calls(self) -> int:
+        return self.calls_collection.count_documents(successful_calls_query)
+
+    def today_successful_calls(self) -> int:
+        return self.calls_collection.count_documents({**successful_calls_query, **self.today_query})
+
+    def failed_calls(self) -> int:
+        return self.calls_collection.count_documents({"status": "failed"})
+
+    def today_failed_calls(self) -> int:
+        return self.calls_collection.count_documents({"status": "failed", **self.today_query})
+
+    def missed_calls(self) -> int:
+        return self.calls_collection.count_documents({"failedReason": "call missed"})
+
+    def today_missed_calls(self) -> int:
+        return self.calls_collection.count_documents({"failedReason": "call missed", **self.today_query})
+
+    def average_call_duration(self) -> int:
+        return Common.seconds_to_duration_str(
+            self.total_duration / self.total_successful_calls
+            if self.total_successful_calls > 0 else 0
+        )
+
+    def scheduled_calls_percentage(self) -> float:
+        successful_scheduled_calls = len(
+            self._get_successful_scheduled_calls_())
+        return round(
+            successful_scheduled_calls / self.total_successful_calls * 100
+            if self.total_successful_calls > 0 else 0, 2
+        )
+
+    def avg_conversation_score(self) -> float:
+        return self._get_avg_conversation_score_()
+
+    def onlineSarathis(self) -> list:
+        query = {"status": "online"}
+        return self.experts_helper.get_experts(query)
+
+    def _get_successful_scheduled_calls_(self) -> list:
+        query = {**successful_calls_query,
+                 "$or": [{"type": "scheduled"}, {"scheduledId": {"$exists": True}}]}
+        projection = {"duration": 1, "scheduledId": 1, "type": 1, "_id": 0}
+        calls = self.common.get_calls(query, projection, False)
+
+        for call in calls:
+            seconds = Common.duration_str_to_seconds(
+                call["duration"]) if "duration" in call else 0
+            if seconds < 60:
+                calls.remove(call)
+        return calls
