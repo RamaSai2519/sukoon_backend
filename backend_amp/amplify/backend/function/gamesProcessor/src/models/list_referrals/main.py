326bcbdcd32b31361f19482da65505b92b115652
Rama Sathya Sai <rchandik@gitam.in>
2024-09-11 14:30:58 +0530

    Add UserReferralService resource for handling user referral actions

---
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/list_referrals/main.py
@@ -0,0 +1,28 @@
+import traceback
+from models.interfaces import Output
+from models.constants import OutputStatus
+from models.list_referrals.compute import Compute
+
+
+class UserReferrals:
+    def __init__(self) -> None:
+        pass
+
+    def process(self) -> Output:
+        try:
+            output = self._compute()
+        except Exception as e:
+            print(traceback.format_exc())
+            output = Output(
+                output_details={},
+                output_status=OutputStatus.FAILURE,
+                output_message=f"{e}",
+            )
+
+        return output
+
+    def _compute(self) -> Output:
+        computation_obj = Compute()
+        output = computation_obj.compute()
+
+        return output
--- backend_amp/amplify/backend/function/gamesProcessor/src/services/src/user_referral.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/src/user_referral.py
@@ -2,16 +2,14 @@ import json
 import dataclasses
 from flask_restful import Resource
 from flask import request
-from models.interfaces import UserReferralInput, Output
-from models.user_referral.main import UserReferral
+from models.interfaces import Output
+from models.list_referrals.main import UserReferrals
 
 
 class UserReferralService(Resource):
-    
-    def post(self) -> Output:
-        input = json.loads(request.get_data())
-        input = UserReferralInput(**input)
-        output = UserReferral(input).process()
+
+    def get(self) -> Output:
+        output = UserReferrals().process()
         output = dataclasses.asdict(output)
 
         return output
