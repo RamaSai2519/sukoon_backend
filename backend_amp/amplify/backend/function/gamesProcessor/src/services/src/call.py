3c58899ba2c1e6d9c201c2b6ad00c5f3dfedff9c
Rama Sathya Sai <rchandik@gitam.in>
2024-09-16 19:32:06 +0530

    Refactor models to include print statement in CallWebhookService post method

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/src/call.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/src/call.py
@@ -22,6 +22,7 @@ class CallWebhookService(Resource):
 
     def post(self) -> dict:
         input = json.loads(request.get_data())
+        print(input)
         input = WebhookInput(**input)
         output = CallWebhook(input).process()
         output = dataclasses.asdict(output)
