127308212ec393dd11f98ef885c9a646c780bcd8
Rama Sathya Sai <rchandik@gitam.in>
2024-09-16 19:31:39 +0530

    Refactor models to remove unused import in call.py

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/src/call.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/src/call.py
@@ -4,7 +4,7 @@ from flask import request
 from flask_restful import Resource
 from models.make_call.main import MakeCall
 from models.call_webhook.main import CallWebhook
-from models.interfaces import CallInput, WebhookInput, Output
+from models.interfaces import CallInput, WebhookInput
 
 
 class CallService(Resource):
