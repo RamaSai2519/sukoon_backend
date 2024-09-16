ff99b9b597b7b93872cb0440ba81add78353bc36
Rama Sathya Sai <rchandik@gitam.in>
2024-09-17 00:46:42 +0530

    Refactor services controller and add new content services

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
@@ -2,12 +2,12 @@ from services.src.admin import AdminFCMService
 from services.src.call import CallWebhookService
 from services.src.shorts import FetchShortsService
 from services.src.game_config import GameConfigService
-from services.src.chat import ChatService, PhotosService
 from services.src.cashfree import CashfreeWebhookService
 from services.src.dashboard import DashboardStatsService
 from services.src.user_referral import UserReferralService
 from services.src.coupon_reward import CouponRewardService
 from services.src.payment import CreatePaymentOrderService
+from services.src.content import ChatService, PhotosService
 from services.src.user import CreateEventUserService, UserService
 from services.src.push_notification import PushNotificationService, FCMTokenService
 from services.src.authenticate import SendOTPService, ValidateOTPService, AdminAuthService
