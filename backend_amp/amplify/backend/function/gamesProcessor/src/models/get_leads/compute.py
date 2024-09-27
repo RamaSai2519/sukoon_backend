bea6a8632afc068c06f62612703e89ebc9cd08fa
Rama Sathya Sai <rchandik@gitam.in>
2024-09-27 10:43:31 +0530

    Refactor get_leads compute module to simplify lead source logic

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/get_leads/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/get_leads/compute.py
@@ -22,9 +22,7 @@ class Compute:
         query = {"user": user.get("_id")}
         user_meta: dict = self.meta_collection.find_one(query)
         if user_meta:
-            user["leadSource"] = user_meta.get("source", "")
-            if str(user["leadSource"]).lower() not in ["events", "zoom"]:
-                user["leadSource"] = "Website"
+            user["leadSource"] = user_meta.get("source", "Website")
         if not user_meta:
             user["leadSource"] = "Website"
 
