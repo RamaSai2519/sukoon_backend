a5aecbd8ba004e6b6ad8bf162a1b490a4ea6e4a3
Rama Sathya Sai <rchandik@gitam.in>
2024-10-03 15:00:58 +0530

    Refactor user service and add engagement data service

---
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/get_engagement_data/compute.py
@@ -0,0 +1,15 @@
+from models.constants import OutputStatus
+from models.interfaces import GetEngagementDataInput as Input, Output
+
+
+class Compute:
+    def __init__(self, input: Input) -> None:
+        self.input = input
+
+    def compute(self) -> Output:
+
+        return Output(
+            output_details={},
+            output_status=OutputStatus.SUCCESS,
+            output_message="Successfully fetched data"
+        )
