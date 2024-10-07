e78283231bb12b247a7020f7fb62d548a31f40f4
Rama Sathya Sai <rchandik@gitam.in>
2024-10-08 01:42:15 +0530

    Refactor call_job_handler.py to update API endpoint and remove unnecessary authorization header

--- backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
+++ backend_amp/amplify/backend/function/scheduler/src/models/call_job_handler.py
@@ -3,18 +3,16 @@ import json
 from models.iprocessor import IProcessor
 from queries.scheduled_job import update_scheduled_job_status
 
+
 class CallJobHandler(IProcessor):
     def __init__(self, job):
         self.request_meta = json.loads(job.get("requestMeta")) or {}
         self.job_id = job.get("id", "")
 
     def invoke_call_backend_api(self):
-        url = "https://prod-backend.sukoonunlimited.com/api/call/backend-make-call"
+        url = "https://6x4j0qxbmk.execute-api.ap-south-1.amazonaws.com/main/actions/call"
 
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
-        }
+        headers = {'Content-Type': 'application/json'}
 
         payload = json.dumps({
             "userId": self.request_meta.get("userId"),
@@ -30,13 +28,7 @@ class CallJobHandler(IProcessor):
     def process_scheduled_job(self):
         response = self.invoke_call_backend_api()
         json_response = response.json()
-        success_status = json_response.get("success")
-        
-        if not success_status:
-            status = ((json_response.get("error", {}) or {}).get("message", {}) or {}).get("message", "")
-        else:
-            status = "Call Placed Successfully"
+        status = json_response.get('output_message')
         update_scheduled_job_status(self.job_id, status)
 
         return response
-
