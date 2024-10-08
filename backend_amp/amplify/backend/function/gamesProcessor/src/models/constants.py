e18f9f1aa6156bf5a567021c3a1f226b2e07ea8f
Rama Sathya Sai <rchandik@gitam.in>
2024-10-08 15:31:33 +0530

    Refactor constants.py and schema.graphql

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/constants.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/constants.py
@@ -1,53 +1,54 @@
 class OutputStatus:
-    SUCCESS = "SUCCESS"
-    FAILURE = "FAILURE"
+    SUCCESS = 'SUCCESS'
+    FAILURE = 'FAILURE'
 
 
 calls_exclusion_projection = {
-    "user": 1,
-    "callId": 1,
-    "expert": 1,
-    "status": 1,
-    "duration": 1,
-    "failedReason": 1,
-    "initiatedTime": 1,
-    "lastModifiedBy": 1,
-    "user_requested": 1,
-    "Conversation Score": 1,
+    'user': 1,
+    'callId': 1,
+    'expert': 1,
+    'status': 1,
+    'duration': 1,
+    'failedReason': 1,
+    'initiatedTime': 1,
+    'recording_url': 1,
+    'lastModifiedBy': 1,
+    'user_requested': 1,
+    'Conversation Score': 1,
 }
 
 
 successful_calls_query = {
-    "failedReason": "",
-    "status": "successfull"
+    'failedReason': '',
+    'status': 'successfull'
 }
 
-application_json_header = {"Content-Type": "application/json"}
+application_json_header = {'Content-Type': 'application/json'}
 
-meta_fields = ["remarks", "expert", "lastReached",
-               "status", "userStatus", "source"]
+meta_fields = ['remarks', 'expert', 'lastReached',
+               'status', 'userStatus', 'source']
 
 indianLanguages = [
-    {"key": "as", "value": "Assamese"},
-    {"key": "bn", "value": "Bengali"},
-    {"key": "brx", "value": "Bodo"},
-    {"key": "doi", "value": "Dogri"},
-    {"key": "gu", "value": "Gujarati"},
-    {"key": "hi", "value": "Hindi"},
-    {"key": "kn", "value": "Kannada"},
-    {"key": "ks", "value": "Kashmiri"},
-    {"key": "kok", "value": "Konkani"},
-    {"key": "mai", "value": "Maithili"},
-    {"key": "ml", "value": "Malayalam"},
-    {"key": "mni", "value": "Manipuri"},
-    {"key": "mr", "value": "Marathi"},
-    {"key": "ne", "value": "Nepali"},
-    {"key": "or", "value": "Odia"},
-    {"key": "pa", "value": "Punjabi"},
-    {"key": "sa", "value": "Sanskrit"},
-    {"key": "sat", "value": "Santali"},
-    {"key": "sd", "value": "Sindhi"},
-    {"key": "ta", "value": "Tamil"},
-    {"key": "te", "value": "Telugu"},
-    {"key": "ur", "value": "Urdu"}
+    {'key': 'as', 'value': 'Assamese'},
+    {'key': 'bn', 'value': 'Bengali'},
+    {'key': 'brx', 'value': 'Bodo'},
+    {'key': 'doi', 'value': 'Dogri'},
+    {'key': 'gu', 'value': 'Gujarati'},
+    {'key': 'hi', 'value': 'Hindi'},
+    {'key': 'kn', 'value': 'Kannada'},
+    {'key': 'ks', 'value': 'Kashmiri'},
+    {'key': 'kok', 'value': 'Konkani'},
+    {'key': 'mai', 'value': 'Maithili'},
+    {'key': 'ml', 'value': 'Malayalam'},
+    {'key': 'mni', 'value': 'Manipuri'},
+    {'key': 'mr', 'value': 'Marathi'},
+    {'key': 'ne', 'value': 'Nepali'},
+    {'key': 'or', 'value': 'Odia'},
+    {'key': 'pa', 'value': 'Punjabi'},
+    {'key': 'sa', 'value': 'Sanskrit'},
+    {'key': 'sat', 'value': 'Santali'},
+    {'key': 'sd', 'value': 'Sindhi'},
+    {'key': 'ta', 'value': 'Tamil'},
+    {'key': 'te', 'value': 'Telugu'},
+    {'key': 'ur', 'value': 'Urdu'}
 ]
