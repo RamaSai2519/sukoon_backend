4ce4e42d312352ab4d60df56fc195568334eb5a4
Rama Sathya Sai <rchandik@gitam.in>
2024-10-16 11:01:51 +0530

    Refactor WASlackNotifier class to use WebClient for sending Slack notifications

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/whatsapp_webhook_event/slack.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/whatsapp_webhook_event/slack.py
@@ -1,34 +1,68 @@
-import requests
-from models.interfaces import WASlackNotifierInput as Input
+import pytz
+from datetime import datetime
+from slack_sdk import WebClient
+from configs import CONFIG as config
+from slack_sdk.errors import SlackApiError
 
 
 class WASlackNotifier:
-    def __init__(self, input: Input) -> None:
-        self.input = input
-        self.webhook_url = "https://hooks.slack.com/services/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"
+    def __init__(self, from_number: str, name: str, body: str) -> None:
+        self.from_number = from_number
+        self.name = name
+        self.body = body
+        self.channel = "C07DYT3RS4T"
+        self.timezone = pytz.timezone("Asia/Kolkata")
+        self.client = WebClient(token=config.SARATHI_SLACK_BOT_TOKEN)
+
+    def join_channel(self) -> None:
+        try:
+            self.client.conversations_join(channel=self.channel)
+        except SlackApiError as e:
+            print(f"Error joining channel: {e}")
 
     def _create_message_blocks(self) -> list:
+        """Compose the message blocks with user information."""
         blocks = [
             {
                 "type": 'section',
                 "text": {
                     "type": 'mrkdwn',
-                    "text": f'<!channel> User with phone number *{self.input.from_number}* of name *{self.input.name}* has sent *{self.input.body}*',
+                    "text": f'<!channel> User with phone number *{self.from_number}* named *{self.name}* has sent *{self.body}*',
                 },
             },
+            {
+                "type": 'section',
+                "fields": [
+                    {
+                        "type": 'mrkdwn',
+                        "text": f'*Time:*\n{datetime.now(self.timezone).strftime("%Y-%m-%d %H:%M:%S")}',
+                    },
+                    {
+                        "type": 'mrkdwn',
+                        "text": f'*Phone Number:*\n{self.from_number}',
+                    },
+                    {
+                        "type": 'mrkdwn',
+                        "text": f'*Message Body:*\n{self.body}',
+                    }
+                ]
+            }
         ]
-
         return blocks
 
-
-    def send_notification(self):
+    def send_notification(self) -> str:
+        """Send the Slack notification with the constructed message."""
         blocks = self._create_message_blocks()
         try:
-            response = requests.post(
-                self.webhook_url, json={"blocks": blocks})
-            if response.status_code == 200:
-                print(f"Notification sent to Slack: {response.text}")
+            self.join_channel()
+            response = self.client.chat_postMessage(
+                channel=self.channel,
+                blocks=blocks,
+                text=f"Notification from {self.name}"
+            )
+            if response["ok"]:
+                return f"Notification sent to Slack"
             else:
-                print(f"Failed to send notification: {response.status_code}, {response.text}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error sending notification: {e}")
+                return f"Failed to send notification: {response}"
+        except SlackApiError as e:
+            return f"Error sending notification: {e.response['error']}"
