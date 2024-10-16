f2ee71f618103070ab83f832ea8eb22e4735b1d8
Rama Sathya Sai <rchandik@gitam.in>
2024-10-16 09:54:07 +0530

    Refactor .gitignore file to exclude unnecessary files and directories

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/call_webhook/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/call_webhook/compute.py
@@ -3,9 +3,9 @@ import requests
 from typing import Union
 from models.common import Common
 from configs import CONFIG as config
-from db.users import get_user_collection
 from db.calls import get_calls_collection
 from db.experts import get_experts_collections
+from db.users import get_user_collection, get_user_notification_collection
 from db_queries.mutations.scheduled_job import update_scheduled_job_status
 from models.constants import OutputStatus, application_json_header, CallStatus
 from models.interfaces import WebhookInput as Input, Call, Output, User, Expert
@@ -19,49 +19,45 @@ class Compute:
         self.calls_collection = get_calls_collection()
         self.url = config.URL + '/actions/send_whatsapp'
         self.experts_collection = get_experts_collections()
-        self.duration_secs = self.common.duration_str_to_seconds(
-            input.call_duration)
+        self.user_notifications_collection = get_user_notification_collection()
+        self.duration_secs = self.common.duration_str_to_seconds(input.call_duration)
 
-    def prep_call(self, call: dict) -> Call:
+    def find_call(self, callId: str) -> Union[Call, None]:
+        call = self.calls_collection.find_one({'callId': callId})
         call = Common.clean_dict(call, Call)
         return Call(**call)
 
-    def find_call(self, callId: str) -> Union[Call, None]:
-        call = self.calls_collection.find_one({"callId": callId})
-        call = self.prep_call(call) if call else None
-        return call
-
     def find_user(self, call: Call) -> Union[User, None]:
-        user = self.users_collection.find_one({"_id": call.user})
+        user = self.users_collection.find_one({'_id': call.user})
         user = Common.clean_dict(user, User)
         return User(**user) if user else None
 
     def find_expert(self, call: Call) -> Union[Expert, None]:
-        expert = self.experts_collection.find_one({"_id": call.expert})
+        expert = self.experts_collection.find_one({'_id': call.expert})
         expert = Common.clean_dict(expert, Expert)
         return Expert(**expert) if expert else None
 
     def update_user(self, call: Call, expert: Expert, user: User) -> str:
-        filter = {"_id": call.user}
-        update_values = {"isBusy": False}
-        if expert and user and expert.type != "internal" and self.duration_secs > 600 and user.numberOfCalls > 0 and user.isPaidUser == False:
-            update_values["numberOfCalls"] = user.numberOfCalls - 1
-        update = {"$set": update_values}
+        filter = {'_id': call.user}
+        update_values = {'isBusy': False}
+        if expert and user and expert.type != 'internal' and self.duration_secs > 600 and user.numberOfCalls > 0 and user.isPaidUser == False:
+            update_values['numberOfCalls'] = user.numberOfCalls - 1
+        update = {'$set': update_values}
         response = self.users_collection.update_one(filter, update)
-        message = "User updated, " if response.modified_count > 0 else "User not updated, "
+        message = 'User updated, ' if response.modified_count > 0 else 'User not updated, '
         return message
 
     def update_expert(self, call: Call) -> str:
-        filter = {"_id": call.expert}
-        update = {"$set": {"isBusy": False}}
+        filter = {'_id': call.expert}
+        update = {'$set': {'isBusy': False}}
         response = self.experts_collection.update_one(filter, update)
-        message = "Expert updated, " if response.modified_count > 0 else "Expert not updated, "
+        message = 'Expert updated, ' if response.modified_count > 0 else 'Expert not updated, '
         return message
 
     def determine_status(self, call_transfer_status: str, call_status: str) -> str:
-        if call_transfer_status == "missed":
+        if call_transfer_status == 'missed':
             status = CallStatus.MISSED
-        elif call_status == "connected":
+        elif call_status == 'connected':
             if self.duration_secs > 120:
                 status = CallStatus.SUCCESSFUL
             else:
@@ -71,14 +67,14 @@ class Compute:
         return status
 
     def determine_failed_reason(self, call_transfer_status: str) -> str:
-        if call_transfer_status == "missed":
-            failed_reason = "user missed"
-        elif call_transfer_status in ["not connected", "none"]:
-            failed_reason = "expert missed"
-        elif call_transfer_status == "did not process":
-            failed_reason = "knowlarity missed"
+        if call_transfer_status == 'missed':
+            failed_reason = 'user missed'
+        elif call_transfer_status in ['not connected', 'none']:
+            failed_reason = 'expert missed'
+        elif call_transfer_status == 'did not process':
+            failed_reason = 'knowlarity missed'
         else:
-            failed_reason = ""
+            failed_reason = ''
         return failed_reason
 
     def determine_failed_reason_and_status(self) -> tuple:
@@ -91,18 +87,18 @@ class Compute:
 
     def update_call(self, call: Call) -> str:
         status, failed_reason = self.determine_failed_reason_and_status()
-        filter = {"callId": call.callId}
+        filter = {'callId': call.callId}
         update = {
-            "$set": {
-                "status": status,
-                "failedReason": failed_reason,
-                "duration": self.input.call_duration,
-                "recording_url": self.input.callrecordingurl,
-                "transferDuration": self.input.call_transfer_duration
+            '$set': {
+                'status': status,
+                'failedReason': failed_reason,
+                'duration': self.input.call_duration,
+                'recording_url': self.input.callrecordingurl,
+                'transferDuration': self.input.call_transfer_duration
             }
         }
         response = self.calls_collection.update_one(filter, update)
-        message = "Call updated, " if response.modified_count > 0 else "Call not updated, "
+        message = 'Call updated, ' if response.modified_count > 0 else 'Call not updated, '
         return message
 
     def update_schedule(self, call: Call) -> str:
@@ -110,54 +106,53 @@ class Compute:
             status, reason = self.determine_failed_reason_and_status()
             status_str = status + ', ' + reason
             update_scheduled_job_status(call.scheduledId, status_str)
-            return "Scheduled job updated, "
-        return "Scheduled job not updated, "
+            return 'Scheduled job updated, '
+        return 'Scheduled job not updated, '
 
     def send_feedback_message(self, call: Call, expert: Expert, user: User) -> str:
-        if call.status == "successful" and self.duration_secs > 120 and expert.type != "internal":
+        if call.status == 'successful' and self.duration_secs > 120 and expert.type != 'internal':
             if not user or not expert:
-                return "Feedback message not sent as user or expert not found"
+                return 'Feedback message not sent as user or expert not found'
             payload = {
-                "template_name": "FEEDBACK_SURVEY",
-                "phone_number": user.phoneNumber,
-                "request_meta": json.dumps({
-                    "sarathiId": str(expert._id),
-                    "callId": call.callId,
-                    "userId": str(user._id)
+                'template_name': 'FEEDBACK_SURVEY',
+                'phone_number': user.phoneNumber,
+                'request_meta': json.dumps({
+                    'sarathiId': str(expert._id),
+                    'callId': call.callId,
+                    'userId': str(user._id)
                 }),
-                "parameters": {"user_name": user.name, "sarathi_name": expert.name}
+                'parameters': {'user_name': user.name, 'sarathi_name': expert.name}
             }
             response = requests.request(
-                "POST", self.url, headers=application_json_header, data=json.dumps(payload))
-            message = "Feedback message sent" if response.status_code == 200 else "Feedback message not sent"
+                'POST', self.url, headers=application_json_header, data=json.dumps(payload))
+            message = 'Feedback message sent' if response.status_code == 200 else 'Feedback message not sent'
             return message
 
     def send_promo_message(self, call: Call, expert: Expert, user: User) -> str:
-        internal_include = self.common.get_internal_exclude_query('true')
-        query = {**internal_include, 'user': user._id}
-        calls_count = self.calls_collection.count_documents(query)
-        if call.status == "successful" and expert.type == "internal" and self.duration_secs > 120 and calls_count > 1:
+        query = {'userId': user._id, 'templateName': 'PROMO_TEMPLATE'}
+        notification = self.user_notifications_collection.find_one(query)
+        if call.status == 'successful' and expert.type == 'internal' and self.duration_secs > 120 and not notification:
             if not user or not expert:
-                return "Promo message not sent as user or expert not found"
+                return 'Promo message not sent as user or expert not found'
             payload = {
-                "template_name": "PROMO_TEMPLATE",
-                "phone_number": user.phoneNumber,
-                "request_meta": json.dumps({"expert_name": expert.name, "callId": call.callId, "user_name": user.name or user.phoneNumber}),
-                "parameters": {'image_link': 'https://sukoon-media.s3.ap-south-1.amazonaws.com/wa_promo_image.jpeg'}
+                'template_name': 'PROMO_TEMPLATE',
+                'phone_number': user.phoneNumber,
+                'request_meta': json.dumps({'expert_name': expert.name, 'callId': call.callId, 'user_name': user.name or user.phoneNumber}),
+                'parameters': {'image_link': 'https://sukoon-media.s3.ap-south-1.amazonaws.com/wa_promo_image.jpeg'}
             }
             response = requests.request(
-                "POST", self.url, headers=application_json_header, data=json.dumps(payload))
-            message = "Promo message sent" if response.status_code == 200 else "Promo message not sent"
+                'POST', self.url, headers=application_json_header, data=json.dumps(payload))
+            message = 'Promo message sent' if response.status_code == 200 else 'Promo message not sent'
             return message
 
     def compute(self) -> Output:
-        callId = self.input.call_uuid.replace("_0", "")
+        callId = self.input.call_uuid.replace('_0', '')
         call = self.find_call(callId)
         if not call:
             return Output(
                 output_details={},
                 output_status=OutputStatus.FAILURE,
-                output_message=f"Call not found with callid: {callId}"
+                output_message=f'Call not found with callid: {callId}'
             )
 
         user = self.find_user(call)
@@ -166,7 +161,7 @@ class Compute:
         schedule_message = self.update_schedule(call)
         user_message = self.update_user(call, expert, user)
         expert_message = self.update_expert(call)
-        feedback_message = "Feedback message not sent"
+        feedback_message = 'Feedback message not sent'
 
         call = self.find_call(callId)
         feedback_message = self.send_feedback_message(call, expert, user)
