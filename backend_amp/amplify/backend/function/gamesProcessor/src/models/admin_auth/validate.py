3450f687ceddf9305c1c5ea4835063e5a19a479b
RamaSai2519 <rchandik@gitam.in>
2024-11-18 10:32:23 +0530

    Enhance input validation in AdminAuthInput and Validator class

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/admin_auth/validate.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/admin_auth/validate.py
@@ -5,7 +5,7 @@ class Validator():
     def __init__(self, input: Input) -> None:
         self.input = input
 
-    def validate_input(self):
+    def validate_input(self) -> tuple:
         if self.input.action not in ["login", "register", "refresh"]:
             return False, "Invalid action"
 
@@ -16,5 +16,7 @@ class Validator():
         if self.input.action == "register":
             if not self.input.phoneNumber or not self.input.password or not self.input.name:
                 return False, "Missing Phone Number or Password or Name"
+            if self.input.access_level not in ["basic", "admin", "super"]:
+                return False, "Invalid access level"
 
         return True, ""
