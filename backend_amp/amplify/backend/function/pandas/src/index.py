547681a37cca4fcf12d787b955257a25f6c26b87
RamaSai2519 <rchandik@gitam.in>
2024-11-18 21:33:51 +0530

    Update bulk user insertion endpoint to use '/pandas/bulk_users' path

--- backend_amp/amplify/backend/function/pandas/src/index.py
+++ backend_amp/amplify/backend/function/pandas/src/index.py
@@ -15,7 +15,7 @@ api = Api(app)
 JWTManager(app)
 CORS(app, supports_credentials=True)
 
-api.add_resource(BulkInsertUsersService, '/actions/bulk_users')
+api.add_resource(BulkInsertUsersService, '/pandas/bulk_users')
 
 
 def handler(event, context):
