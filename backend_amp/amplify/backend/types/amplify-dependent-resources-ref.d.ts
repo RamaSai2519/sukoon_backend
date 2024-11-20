9755499a8795de13a0b05cafaf4b715d2d231873
RamaSai2519 <rchandik@gitam.in>
2024-11-20 15:18:14 +0530

    Add LambdaExecutionRoleArn to pandas resource and update deployment configurations

--- backend_amp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ backend_amp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -29,6 +29,7 @@ export type AmplifyDependentResourcesAttributes = {
     "pandas": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
     },
