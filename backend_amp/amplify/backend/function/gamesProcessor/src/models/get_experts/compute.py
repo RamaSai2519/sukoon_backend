c84efd0d17244f0e567adb66b69742549e1e65c0
RamaSai2519 <rchandik@gitam.in>
2024-12-05 22:26:52 +0530

    Refactor Compute classes: remove unused create_excel method and enhance expert filtering with category support

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/get_experts/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/get_experts/compute.py
@@ -1,6 +1,7 @@
+from shared.models.constants import OutputStatus, exclude_deleted_query
 from shared.models.interfaces import GetExpertsInput as Input, Output
+from shared.db.experts import get_categories_collection
 from shared.db.calls import get_schedules_collection
-from shared.models.constants import OutputStatus
 from shared.helpers.experts import ExpertsHelper
 from shared.models.common import Common
 from bson import ObjectId
@@ -12,6 +13,7 @@ class Compute:
         self.common = Common()
         self.helper = ExpertsHelper()
         self.schedules_collection = get_schedules_collection()
+        self.categories_collection = get_categories_collection()
 
     def populate_schedules(self, expert: dict) -> dict:
         query = {"expert": ObjectId(
@@ -37,9 +39,18 @@ class Compute:
         else:
             query = self.common.get_internal_exclude_query(
                 self.input.internal, "_id")
-            exclude_deleted_query = {
-                "$or": [{"isDeleted": {"$exists": False}}, {"isDeleted": False}]
-            }
+            if self.input.filter_field == 'categories':
+                filter_query = self.common.get_filter_query(
+                    'name', self.input.filter_value)
+                cats = list(self.categories_collection.find(filter_query))
+                cat_ids = [str(cat['_id']) for cat in cats]
+                filter_query = {'categories': {'$in': cat_ids}}
+                query = {**filter_query, **query}
+            else:
+                filter_query = self.common.get_filter_query(
+                    self.input.filter_field, self.input.filter_value)
+                query = {**filter_query, **query}
+
             query = {**query, **exclude_deleted_query}
             experts = self.helper.get_experts(query)
 
