393110d35dc61e748f3861d3446472fbca8867af
RamaSai2519 <rchandik@gitam.in>
2024-12-06 10:12:26 +0530

    Refactor Compute class: update webhook verification logic to use config token and improve readability

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/verify_whatsapp_webhook/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/verify_whatsapp_webhook/compute.py
@@ -1,14 +1,15 @@
 from shared.models.interfaces import GetWhatsappWebhookInput as Input
+from shared.configs import CONFIG as config
 
 
 class Compute:
     def __init__(self, input: Input) -> None:
         self.input = input
 
-    def compute(self):
+    def compute(self) -> str:
 
-        verify_token = self.input.hub_verify_token
-        if verify_token == "sukoon_verification":
-            return self.input.hub_challenge
+        if self.input.hub_mode and self.input.hub_verify_token == config.WHATSAPP_API['ACCESS_TOKEN']:
+            if self.input.hub_mode == "subscribe":
+                return self.input.hub_challenge
 
         return "Not able to verify webhook"
