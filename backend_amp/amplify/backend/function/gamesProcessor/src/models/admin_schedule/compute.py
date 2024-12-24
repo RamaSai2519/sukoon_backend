b045585343dd962d24bf79f589cc8f0676105982
RamaSai2519 <rchandik@gitam.in>
2024-12-24 21:37:27 +0530

    Remove unused format_schedules method and clean up subproject commits

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/admin_schedule/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/admin_schedule/compute.py
@@ -13,26 +13,6 @@ class Compute:
         self.common = Common()
         self.collection = get_schedules_collection()
 
-    def format_schedules(self, schedules: List[Dict]):
-        for schedule in schedules:
-            request_meta: str = schedule.get('requestMeta', None)
-            user_requested = schedule.get('user_requested', None)
-            if request_meta is not None:
-                request_meta: dict = json.loads(request_meta)
-
-                expert_id = request_meta.get('expertId')
-                schedule['expert'] = self.common.get_expert_name(
-                    ObjectId(expert_id)) if expert_id else None
-
-                user_id = request_meta.get('userId')
-                schedule['user'] = self.common.get_user_name(
-                    ObjectId(user_id)) if user_id else None
-                schedule['initiatedBy'] = request_meta.get('initiatedBy', '')
-
-            schedule['datetime'] = schedule.get('scheduledJobTime')
-            schedule['source'] = Common.get_call_source(user_requested)
-        return {'data': schedules}
-
     def __format__(self, schedules: list) -> list:
         for schedule in schedules:
             job = Common.clean_dict(schedule, Schedule)
@@ -74,7 +54,6 @@ class Compute:
 
     def get_schedules(self) -> list:
         query = self.prep_query()
-        print(query, 'query')
         cursor = self.collection.find(query).sort('job_time', -1)
         cursor = Common.paginate_cursor(cursor, int(
             self.input.page), int(self.input.size))
