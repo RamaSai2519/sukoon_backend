2f0f1813932d979ce9ab7b1c32c3c31d4f884e58
RamaSai2519 <rchandik@gitam.in>
2024-12-26 13:56:22 +0530

    Add event validation to input in Validator class

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_event_user/validate.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_event_user/validate.py
@@ -1,9 +1,16 @@
 from shared.models.interfaces import EventUserInput as Input
+from shared.db.events import get_events_collection
 
 
 class Validator():
     def __init__(self, input: Input) -> None:
         self.input = input
+        self.events_collection = get_events_collection()
 
-    def validate_input(self):
+    def validate_input(self) -> tuple:
+        query = {'slug': self.input.source}
+        event = self.events_collection.find_one(query)
+        if not event:
+            return False, "Invalid source"
+
         return True, ""
