c7f3ac4065e2caffb0da8b768f382c08e3c26ba8
RamaSai2519 <rchandik@gitam.in>
2025-01-11 12:39:18 +0530

    Add prompt proposal handling in compute.py for upsert functionality

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_prompt/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_prompt/compute.py
@@ -1,4 +1,4 @@
-from shared.db.chat import get_system_prompts_collection, get_histories_collection
+from shared.db.chat import get_system_prompts_collection, get_histories_collection, get_prompt_proposals_collection
 from shared.models.interfaces import UpsertPromptInput as Input, Output
 from shared.models.constants import OutputStatus
 from shared.models.common import Common
@@ -9,21 +9,55 @@ class Compute:
         self.input = input
         self.histories_collection = get_histories_collection()
         self.system_prompts_collection = get_system_prompts_collection()
+        self.prompt_proposals_collection = get_prompt_proposals_collection()
 
     def compute(self) -> Output:
-        prompt = self.system_prompts_collection.find_one_and_update(
+        if self.input.auth == 196970:
+            query = {"context": self.input.context}
+            doc = self.prompt_proposals_collection.find_one(query)
+            if doc:
+                prompt = self.system_prompts_collection.update_one(
+                    query, {
+                        "$set": {
+                            "content": doc.get(
+                                "content", self.input.content)
+                        }
+                    }
+                )
+            else:
+                prompt = self.system_prompts_collection.insert_one(
+                    {
+                        "context": self.input.context,
+                        "content": self.input.content
+                    }
+                )
+
+            # prompt = self.system_prompts_collection.find_one_and_update(
+            #     filter={"context": self.input.context},
+            #     update={"$set": {"content": self.input.content}},
+            #     upsert=True,
+            #     return_document=True
+            # )
+
+            # if self.input.context == 'ark_main':
+            #     now_date = Common.get_current_utc_time()
+            #     now_date = now_date.strftime('%Y-%m-%d')
+            #     query = {'createdAt': now_date}
+            #     update = {'$set': {'history.0.content': self.input.content}}
+            #     self.histories_collection.update_many(query, update)
+
+            return Output(
+                output_details=Common.jsonify(prompt),
+                output_status=OutputStatus.SUCCESS,
+                output_message="Successfully upserted prompt"
+            )
+
+        prompt = self.prompt_proposals_collection.find_one_and_update(
             filter={"context": self.input.context},
             update={"$set": {"content": self.input.content}},
             upsert=True,
             return_document=True
         )
-
-        if self.input.context == 'ark_main':
-            now_date = Common.get_current_utc_time()
-            now_date = now_date.strftime('%Y-%m-%d')
-            query = {'createdAt': now_date}
-            update = {'$set': {'history.0.content': self.input.content}}
-            self.histories_collection.update_many(query, update)
 
         return Output(
             output_details=Common.jsonify(prompt),
