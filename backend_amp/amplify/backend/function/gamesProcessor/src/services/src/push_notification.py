0a170c609d1b55c4b2d31b337ababda146bd587b
RamaSai2519 <rchandik@gitam.in>
2025-01-15 13:10:51 +0530

    Refactor FCM template processing logic and update subproject commits

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/src/push_notification.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/src/push_notification.py
@@ -1,11 +1,13 @@
 import json
 import dataclasses
-from flask_restful import Resource
 from flask import request
-from shared.models.interfaces import PushNotificationInput, Output, UpdateFCMTokenInput, UpsertFCMTemplateInput
-from models.send_push_notification.main import SendPushNotification
+from flask_restful import Resource
+from models.get_fcm_templates.main import GetFCMTemplates
 from models.update_fcm_token.main import UpdateUserFCMToken
 from models.upsert_fcm_template.main import UpsertFCMTemplate
+from models.send_push_notification.main import SendPushNotification
+from shared.models.interfaces import PushNotificationInput, Output, UpdateFCMTokenInput, UpsertFCMTemplateInput
+
 
 class PushNotificationService(Resource):
 
@@ -29,16 +31,16 @@ class FCMTokenService(Resource):
 
         return output
 
+
 class FCMTemplateService(Resource):
 
     def post(self) -> dict:
         input = json.loads(request.get_data())
         input = UpsertFCMTemplateInput(**input)
-        output= UpsertFCMTemplate(input).process()
+        output = UpsertFCMTemplate(input).process()
         output = dataclasses.asdict(output)
 
         return output
 
     def get(self) -> dict:
         output = UpsertFCMTemplate().process()
-        
