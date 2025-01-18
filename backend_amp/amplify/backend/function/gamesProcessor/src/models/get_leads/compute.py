3b1b170181b1fe68a8509131d89b24e9d486b939
RamaSai2519 <rchandik@gitam.in>
2025-01-18 20:10:14 +0530

    Refactor lead retrieval logic in compute.py: streamline queries and enhance call user filtering

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/get_leads/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/get_leads/compute.py
@@ -1,48 +1,67 @@
 from shared.models.common import Common
-from shared.models.constants import OutputStatus
+from shared.db.users import get_user_collection
+from shared.db.calls import get_calls_collection
 from shared.models.interfaces import GetLeadsInput as Input, Output
-from shared.db.users import get_user_collection, get_meta_collection
 
 
 class Compute:
     def __init__(self, input: Input) -> None:
         self.input = input
-        self.meta_collection = get_meta_collection()
         self.users_collection = get_user_collection()
-        #self.today_query= Common.get_today_query("CreatedDate")
+        self.calls_collection = get_calls_collection()
 
-    def get_user_query(self, leads: bool) -> dict:
-        query = {"profileCompleted": not leads}
-        if leads and self.input.filter_field and self.input.filter_value:
-            query[self.input.filter_field] = {
-                "$regex": self.input.filter_value, "$options": "i"}
-        return query
+    def get_leads(self, query: dict) -> dict:
+        cursor = self.users_collection.find(query).sort(
+            self.input.sort_field, int(self.input.sort_order))
+        paginated_cursor = Common.paginate_cursor(
+            cursor, int(self.input.page), int(self.input.size))
+        users = list(paginated_cursor)
+        return Output(
+            output_details={
+                'total': self.users_collection.count_documents(query),
+                'data': users
+            })
 
-    def __format__(self, user: dict) -> dict:
-        query = {"user": user.get("_id")}
-        user_meta: dict = self.meta_collection.find_one(query)
-        user["leadSource"] = user_meta.get(
-            "source", "Website") if user_meta else "Website"
-        return Common.jsonify(user)
+    def get_call_users(self, number_of_calls: int) -> dict:
+        pipeline = [
+            {"$match": {"status": "successful"}},
+            {"$group": {"_id": "$user", "call_count": {"$sum": 1}}},
+            {"$match": {"call_count": 0}},
+            {"$group": {
+                "_id": "data",
+                "unique_user_ids": {"$addToSet": "$_id"}
+            }}
+        ]
+        if number_of_calls == 1:
+            pipeline[2]['$match']['call_count'] = 1
+        elif number_of_calls == 2:
+            pipeline[2]['$match']['call_count'] = 2
+        elif number_of_calls == 3:
+            pipeline[2]['$match']['call_count'] = {"$gt": 2}
+        data = list(self.calls_collection.aggregate(pipeline))
+        users = data[0]['unique_user_ids']
+        return {'_id': {'$in': users}}
 
-    def get_leads(self, query: dict) -> list:
-        projection = {"customerPersona": 0}
-        sort_order = self.input.sort_order if self.input.sort_order else -1
-        sort_field = self.input.sort_field if self.input.sort_field else "createdDate"
+    def prep_query(self) -> dict:
+        query = {'profileCompleted': False}
+        filter_query = Common.get_filter_query(
+            self.input.filter_field, self.input.filter_value)
+        query.update(filter_query)
+        if self.input.type == 'total_leads':
+            return query
+        elif self.input.type == 'one_call_users':
+            spec_query = self.get_call_users(1)
+            query.update(spec_query)
+            return query
+        elif self.input.type == 'two_call_users':
+            spec_query = self.get_call_users(2)
+            query.update(spec_query)
+            return query
+        elif self.input.type == 'repeat_call_users':
+            spec_query = self.get_call_users(3)
+            query.update(spec_query)
+            return query
 
-        cursor = self.users_collection.find(query, projection)
-        sorted_cursor = cursor.sort(sort_field, int(sort_order))
-
-        page, size = int(self.input.page), int(self.input.size)
-        user_leads = list(Common.paginate_cursor(sorted_cursor, page, size))
-        return [self.__format__(user) for user in user_leads]
-
-    def compute(self) -> Output:
-        leads_query = self.get_user_query(leads=True)
-        output_data = {}
-
-        if self.input.data:
-            leads = self.get_leads(leads_query)
-            output_data["data"] = leads
-
-        return Output(output_details=output_data)
+    def compute(self) -> dict:
+        query = self.prep_query()
+        return self.get_leads(query)
