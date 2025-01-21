5042bc9f7c89654cb29e3427a08d50855975a216
RamaSai2519 <rchandik@gitam.in>
2025-01-21 14:21:54 +0530

    Add stcaller function with configuration, routes, and initial services

--- backend_amp/amplify/backend/function/stcaller/src/index.py
+++ backend_amp/amplify/backend/function/stcaller/src/index.py
@@ -12,3 +12,21 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = config.JWT_SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = config.JWT_ACCESS_TOKEN_EXPIRES
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = config.JWT_REFRESH_TOKEN_EXPIRES
+
+api = Api(app)
+JWTManager(app)
+CORS(app, supports_credentials=True)
+
+
+@app.after_request
+def handle_after_request(response: Response) -> Response:
+    return Handler(response).handle_after_request()
+
+
+def handler(event, context) -> dict:
+    print(event)
+    return awsgi.response(app, event, context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8070)
