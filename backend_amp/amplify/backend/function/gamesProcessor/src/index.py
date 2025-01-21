c263da91ffed5dcd5d70efd425c37c46f09e4d89
RamaSai2519 <rchandik@gitam.in>
2025-01-21 18:34:07 +0530

    Refactor Flask app to remove debug mode and update subproject commits across multiple services

--- backend_amp/amplify/backend/function/gamesProcessor/src/index.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/index.py
@@ -7,7 +7,6 @@ from flask_jwt_extended import JWTManager
 from shared.configs import CONFIG as config
 from shared.uniservices.after_request import Handler
 
-
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = config.JWT_SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = config.JWT_ACCESS_TOKEN_EXPIRES
@@ -130,4 +129,4 @@ def handler(event, context) -> dict:
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(port=8080)
