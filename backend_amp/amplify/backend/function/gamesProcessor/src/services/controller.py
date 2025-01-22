3762d43acef00b887e0a3e342f172b317a459249
RamaSai2519 <rchandik@gitam.in>
2025-01-23 00:03:14 +0530

    Add ValidateRefTokenService to user referral imports in controller.py

--- backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/services/controller.py
@@ -9,12 +9,12 @@ from services.src.categories import PlatformCategoryService
 from services.src.leads import LeadsCountService, LeadsService
 from services.src.admin import AdminFCMService, UploadService, LogsService
 from services.src.scheduled_jobs import ReSchedulesService, SchedulesService
-from services.src.user_referral import UserReferralService, UpsertOfferService
 from services.src.call import CallWebhookService, CallService, EscalationService
 from services.src.authenticate import SendOTPService, ValidateOTPService, AdminAuthService
 from services.src.content import ChatService, PhotoService, ContentService, DallImageService
 from services.src.mark import UpdateExpertScoresService, SystemPromptsService, HistoriesService
 from services.src.subscription import UserBalanceService, CheckEligibilityService, SubPlanService
+from services.src.user_referral import UserReferralService, UpsertOfferService, ValidateRefTokenService
 from services.src.push_notification import PushNotificationService, FCMTokenService, FCMTemplateService
 from services.src.expert import ExpertService, ApplicantService, SlotsService, TimingService, CategoryService, AgentMetaService
 from services.src.game import QuizGameService, CardGameService, ScoreUpdaterService, CalculateWinnerService, GameHistoryService, BytePlusTokenService
