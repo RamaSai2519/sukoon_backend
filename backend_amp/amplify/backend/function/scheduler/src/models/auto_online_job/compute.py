554987a2b4a1191902d1a3e214ba0255e8fad2b5
RamaSai2519 <rchandik@gitam.in>
2025-01-30 12:42:35 +0530

    Add vacation check for experts in Compute class

--- backend_amp/amplify/backend/function/scheduler/src/models/auto_online_job/compute.py
+++ backend_amp/amplify/backend/function/scheduler/src/models/auto_online_job/compute.py
@@ -1,4 +1,4 @@
-from shared.db.experts import get_experts_collections, get_timings_collection
+from shared.db.experts import get_experts_collections, get_timings_collection, get_vacations_collection
 from shared.models.constants import TimeFormats
 from shared.models.interfaces import Output
 from shared.configs import CONFIG as config
@@ -13,6 +13,7 @@ class Compute:
         self.collection = get_experts_collections()
         self.now_time = Common.get_current_ist_time()
         self.timings_collection = get_timings_collection()
+        self.vacations_collection = get_vacations_collection()
 
     def job(self, expert_id: ObjectId, timing_id: ObjectId) -> bool:
         target_time = Common.get_current_utc_time() + timedelta(hours=1)
@@ -35,6 +36,26 @@ class Compute:
             return True
         return False
 
+    def check_vacation(self, expert_id: ObjectId, hour: str) -> bool:
+        query = {}
+        query['user'] = expert_id
+        current_time = Common.get_current_utc_time()
+        query['start_date'] = {'$lte': current_time}
+        query['end_date'] = {'$gte': current_time}
+        doc = self.vacations_collection.find_one(query)
+        if not doc:
+            return True
+        start_time: str = doc.get('start_time', '')
+        end_time: str = doc.get('end_time', '')
+        if start_time != '' and end_time != '':
+            start_hour = start_time.split(':')[0]
+            end_hour = end_time.split(':')[0]
+            hour = hour.split(':')[0]
+            if int(start_hour) <= int(hour) <= int(end_hour):
+                print('Expert:', expert_id, 'is on vacation')
+                return False
+        return True
+
     def compute(self) -> Output:
         today = self.now_time.strftime("%A")
         ctime = self.now_time.replace(minute=0)
@@ -53,6 +74,8 @@ class Compute:
         for doc in docs:
             expert_id = doc['expert']
             timing_id = doc['_id']
+            if not self.check_vacation(expert_id, hour):
+                continue
             job_status = self.job(expert_id, timing_id)
             if job_status:
                 print('Expert: ', expert_id, ' is online')
