8bd965b5300421fe41d976605e4f4e4188d928ec
RamaSai2519 <rchandik@gitam.in>
2025-02-09 18:04:00 +0530

    Add timezone awareness to start date validation in Validator class

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_user_vacation/validate.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_user_vacation/validate.py
@@ -3,6 +3,7 @@ from shared.models.constants import TimeFormats
 from shared.models.common import Common
 from datetime import datetime
 from bson import ObjectId
+import pytz
 
 
 class Validator:
@@ -12,6 +13,7 @@ class Validator:
     def validate_start_data(self) -> bool:
         start_date = datetime.strptime(self.input.start_date,
                                        TimeFormats.ANTD_TIME_FORMAT)
+        start_date = start_date.replace(tzinfo=pytz.utc)
         current_time = Common.get_current_utc_time()
         if start_date < current_time:
             return False, "Start date cannot be in the past"
@@ -26,9 +28,8 @@ class Validator:
         try:
             fields = ['start_date', 'end_date']
             for field in fields:
-                print(getattr(self.input, field))
-                datetime.strptime(getattr(self.input, field),
-                                  TimeFormats.ANTD_TIME_FORMAT)
+                date = getattr(self.input, field)
+                datetime.strptime(date, TimeFormats.ANTD_TIME_FORMAT)
         except:
             return False, "Invalid date(s)"
 
@@ -43,8 +44,8 @@ class Validator:
         if self.input.user_type not in ['user', 'expert']:
             return False, "Invalid user type"
 
-        valid, message = self.validate_start_data()
-        if not valid:
-            return valid, message
+        # valid, message = self.validate_start_data()
+        # if not valid:
+        #     return valid, message
 
         return True, ""
