242f0ddd694671b4bd382a0b1a6af4f8f1364bd3
RamaSai2519 <rchandik@gitam.in>
2025-02-11 15:16:37 +0530

    Add estimated read time calculation to blog post processing

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_blogposts/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_blogposts/compute.py
@@ -1,9 +1,10 @@
 import requests
+from shared.models.common import Common
 from shared.configs import CONFIG as config
+from shared.models.constants import OutputStatus
 from shared.db.content import get_blogposts_collection
 from shared.models.interfaces import BlogPostInput as Input, Output
-from shared.models.constants import OutputStatus
-from shared.models.common import Common
+
 
 class Compute:
     def __init__(self, input: Input) -> None:
@@ -16,7 +17,7 @@ class Compute:
             "context": "blog",
             "phoneNumber": "1234567890"
         }
-        url = config.URL + '/actions/chat' 
+        url = config.URL + '/actions/chat'
         response = requests.post(url, json=payload)
         data = response.json().get('output_details', {})
 
@@ -32,12 +33,16 @@ class Compute:
         if not new_data.get("description") or not new_data.get("meta_description") or not new_data.get("tags"):
             generated_data = self.generate_missing_fields(
                 title=new_data.get("title"),
-                body=new_data.get("body")
+                body=new_data.get("content_html")
             )
 
             new_data["description"] = new_data.get("description") or generated_data.get("description")
             new_data["meta_description"] = new_data.get("meta_description") or generated_data.get("meta_description")
             new_data["tags"] = new_data.get("tags") or generated_data.get("tags")
+
+        if self.input.number_of_words:
+            est_read_time = round(self.input.number_of_words / 200)
+            new_data["estimated_read_time"] = est_read_time
 
         query = {"title": new_data.get("title")}
         blogpost = self.blogposts_collection.find_one_and_update(
