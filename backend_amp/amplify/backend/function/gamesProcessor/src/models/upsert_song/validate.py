28df259224eaf06db1d7c30162405a218d492af4
RamaSai2519 <rchandik@gitam.in>
2025-02-11 17:43:47 +0530

    Add user type validation in Validator class for upserting songs

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_song/validate.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/upsert_song/validate.py
@@ -1,10 +1,15 @@
+from shared.db.users import get_admins_collection, get_user_collection
 from shared.models.interfaces import UpsertSongInput as Input
+from shared.db.experts import get_experts_collections
 from bson import ObjectId
 
 
 class Validator:
     def __init__(self, input: Input) -> None:
         self.input = input
+        self.users_collection = get_user_collection()
+        self.admins_collection = get_admins_collection()
+        self.experts_collection = get_experts_collections()
 
     def validate_input(self) -> tuple:
         if not self.input.audio_url.startswith('https://') or not self.input.audio_url.endswith('.mp3'):
@@ -23,5 +28,21 @@ class Validator:
             ObjectId(self.input.user_id)
         except:
             return False, 'user_id must be a valid ObjectId'
+
+        if self.input.user_type == 'user':
+            query = {'_id': ObjectId(self.input.user_id)}
+            user = self.users_collection.find_one(query)
+            if not user:
+                return False, 'user_id does not exist'
+        elif self.input.user_type == 'expert':
+            query = {'_id': ObjectId(self.input.user_id)}
+            expert = self.experts_collection.find_one(query)
+            if not expert:
+                return False, 'expert_id does not exist'
+        else:
+            query = {'_id': ObjectId(self.input.user_id)}
+            admin = self.admins_collection.find_one(query)
+            if not admin:
+                return False, 'admin_id does not exist'
 
         return True, ''
