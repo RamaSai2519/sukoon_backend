55a559b4fc8d76012a4606ceedff889b8591b92a
RamaSai2519 <rchandik@gitam.in>
2025-03-28 13:03:33 +0530

    Add event reminder functionality for users in compute.py

--- backend_amp/amplify/backend/function/gamesProcessor/src/models/event_webhook/compute.py
+++ backend_amp/amplify/backend/function/gamesProcessor/src/models/event_webhook/compute.py
@@ -8,7 +8,7 @@ class Compute:
     def __init__(self, input: Input) -> None:
         self.input = input
 
-    def queue_wa_msgs(self):
+    def queue_wa_msgs_for_joined_users(self):
         payload = {
             'action': 'send',
             'event_joined': True,
@@ -24,13 +24,33 @@ class Compute:
             return False
         return True
 
+    def remind_users(self) -> bool:
+        payload = {
+            'action': 'send',
+            'event_joined': True,
+            'initiatedBy': 'webhook',
+            'event_id': self.input.slug,
+            'template': 'EVENT_REMINDER_NOT_JOINED',
+            'params': {
+                'user_name': 'User',
+                'main_title': self.input.main_title,
+                'link': f'https://event.sukoonunlimited.com/d/l?slug={self.input.slug}'}
+        }
+        url = config.MARK_URL + '/flask/queue_wa_msgs'
+        response = requests.post(url, json=payload)
+        print(response.json())
+        if response.status_code != 200:
+            return False
+        return True
+
     def compute(self) -> Output:
-        response = self.queue_wa_msgs()
-        if not response:
-            return Output(
-                output_status=OutputStatus.FAILURE,
-                output_message="Failed to queue wa msgs",
-            )
+        if self.input.event_ended:
+            response = self.queue_wa_msgs_for_joined_users()
+            if not response:
+                return Output(
+                    output_status=OutputStatus.FAILURE,
+                    output_message="Failed to queue wa msgs",
+                )
 
         return Output(
             output_message="Successfully queued wa msgs",
